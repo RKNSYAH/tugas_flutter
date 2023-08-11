20ea25317fa9928c24be4bb27ed6f3d98950082a
RKNSYAH <115148049+RKNSYAH@users.noreply.github.com>
2023-08-12 00:24:57 +0700

    Add files via upload

--- interactive-widget/lib/list.dart
+++ interactive-widget/lib/list.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
 class DaftarKomposisi extends StatefulWidget {
   final String namaMinuman;
   final int harga;
